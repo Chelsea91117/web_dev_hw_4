2aa17d4a691c453f176ad287c90910012da7e623
Chelsea91117 <aidana.latifova@yandex.kz>
2024-07-24 23:16:39 +0200

    Update data

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey, Float
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey, Float, func
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
 Base = declarative_base()
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine('sqlite:///:memory:', echo=False)
 Session = sessionmaker(bind=engine)
 sync_session = Session()
 
@@ -24,11 +24,11 @@ class Product(Base):
     category_id = Column(Integer, ForeignKey('categories.id'))
     category = relationship('Category', back_populates='products')
 
-
-
-
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
+
+# Задача 1: Наполнение данными
+# Добавьте в базу данных следующие категории и продукты
 
 category_1 = Category(title="Electronics", description="Gadgets and devices")
 category_2 = Category(title="Books", description="Printed books and e-books")
@@ -43,3 +43,32 @@ product_4 = Product(title='Jeans', price=40.50, is_available=True, category_id=c
 product_5 = Product(title='T-shirt', price=20.00, is_available=True, category_id=category_3.id)
 sync_session.add_all([product_1, product_2, product_3, product_4, product_5])
 sync_session.commit()
+
+# Задача 2: Чтение данных
+# Извлеките все записи из таблицы categories.
+# Для каждой категории извлеките и выведите все связанные с ней продукты, включая их названия и цены.
+
+# categories = sync_session.query(Category).all()
+# for category in categories:
+#     print(f"Category: {category.title} - {category.description}")
+#     for product in category.products:
+#         print(f"    Product: {product.title}, Price: {product.price}")
+
+# Задача 3: Обновление данных
+# Найдите в таблице products первый продукт с названием "Смартфон". Замените цену этого продукта на 349.99.
+
+# product_s = sync_session.query(Product).filter_by(title="Smartphone").first()
+# if product_s:
+#     product_s.price = 349.99
+#     sync_session.commit()
+#     print(f'{product_s.title} has new price: {product_s.price}')
+# else:
+#     print('Was not found')
+
+# Задача 4: Агрегация и группировка
+# Используя агрегирующие функции и группировку, подсчитайте общее количество продуктов в каждой категории.
+
+# total_products = sync_session.query(Product.category_id, func.count(Product.id).label('product_count')).group_by(Product.category_id).all()
+# for category_id, product_count in total_products:
+#     print(f"Category ID: {category_id}, Total Products: {product_count}")
+
